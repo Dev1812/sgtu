2781fb02adde485b2cd78337f6a089e53f29034c
queue_18 <timka_issaev@mail.ru>
2016-10-16 19:09:18 +0300

    updated controller, rename function getNews

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -12,14 +12,14 @@ class Controller_Main extends Controller {
     $i18n = $this->i18n->get(array('welcome', 'news_not_found', 'read_more', 'welcome_title', 'welcome_text'));
     $param['css'] = array('index');
     $param['title'] = $i18n['welcome'];
-    $data['news'] = $this->model->getPosts();
+    $data['news'] = $this->model->getNews();
     $this->view->generate('main_view.php', 'template_view.php', $param, $data, $i18n);
   }
 
   function action_get() {
     $lang = $this->i18n->get(array('post'));
     $data['post'] = $this->model->getPost($_GET['news_id']);
-	$title = str_replace($lang['post'], '%title%', $data['post']['title']);
+	  $title = str_replace($lang['post'], '%title%', $data['post']['title']);
     $param['title'] = $title;
     $this->view->generate('post_view.php', 'template_view.php', $param, $data);
   }
