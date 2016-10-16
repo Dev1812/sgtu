71bcc3e09b432d2c7ed4641a5ed1ae645f69499c
queue_18 <timka_issaev@mail.ru>
2016-10-16 13:45:06 +0300

    updated files

--- application/core/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-class DataBase{
-  public static function connect($host = 'localhost', $user = 'root', $password = '', $db_name = 'sgtu') {
-    return new PDO('mysql:host='.$host.';dbname='.$db_name, $user, $password);
-  }
-}
-?>
