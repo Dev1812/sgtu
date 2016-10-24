b38185a97d7776fc9541a868dd37c4277941ee4e
queue_18 <timka_issaev@mail.ru>
2016-10-24 21:19:13 +0300

    registration improvements and bug fixes

--- application/controllers/controller_reg.php
+++ application/controllers/controller_reg.php
@@ -1,44 +1,37 @@
 <?php
 class Controller_Reg extends Controller {
-    
-  function __construct() {
+   
+   public $i18n;
+   public $view;
+   public $security;
+   public $model;
+
+  public function __construct() {
     $this->i18n = new i18n;
     $this->view = new View;
+    $this->security = new security;
     $this->model = new Model_Reg;
+    $this->security->setSessionName('reg_hash');
   }
 
-  function action_index() {
-    if($_POST['reg']) {
-      $this->reg();
-      return false;
-    }
+  public function action_index() {
+    $p = $_POST;
     $i18n = $this->i18n->get(array('reg','reg_text','your_name','your_lastname','your_email','your_password','register'));
     $param['css'] = array('reg');
     $param['title'] = $i18n['reg'];
-    $this->view->generate('reg_view.php', 'template_view.php', $param, $data, $i18n);
-  }
-
-  public function reg() {
-    /*if(!$this->security->checkCSRFToken($_POST['form_hash'])) {
-      return array('err'=>true,'err_msg'=>'Ошибка');
-    }*/
-    $reg = $this->model->reg($_POST['firstname'],$_POST['lastname'],$_POST['email'],$_POST['password']);
-    if($_POST['ajax'] == 1) {
-      echo json_encode($reg);
-      return false;
-    } else {
-      if($reg['err'] == false) {
-        if($reg['url']) {
-          header('Location: '.$reg['url']);
-        } 
+    
+    if(isset($p['reg_submit'])) {
+      if(!$this->security->checkCSRFToken($p['form_hash'])){
+        // incorrect captcha code
+        $data['reg'] = array('err'=>true, 'err_msg'=>$this->i18n->get('unknown_error'));
       } else {
-        if($reg['err_msg']) {
-          $data['reg_msg'] = $reg['err_msg'];
-        }
+        // captcha code is correct, trying to call the Reg model
+        $data['reg'] = $this->model->reg($p['first_name'], $p['last_name'], $p['email'], $p['password']);
       }
     }
-    $this->view->generate('reg_view.php', 'template_view.php', $param, $data, $i18n);
 
+    $data['form_hash'] = $this->security->getCSRFToken();
+    $this->view->generate('reg_view.php', 'template_view.php', $param, $data, $i18n);
   }
 
 }
