352682c6c5b132b3b1934d1519bf94ca3c319b6c
queue_18 <timka_issaev@mail.ru>
2016-10-24 23:35:08 +0300

    fixed a few bugs, updated file

--- application/core/i18n.php
+++ application/core/i18n.php
@@ -1,52 +1,52 @@
 <?php
-class i18n {
+  class i18n {
 
-  private $lang;
+    private $lang;
 
-  /**
-   * @example array('lang' => array('key' => 'value'))
-   *
-   */
-  public $cache = array();
+    /**
+     * @example array('lang' => array('key' => 'value'))
+     *
+     */
+    public $cache = array();
 
 
-  public function __construct() {
-    $this->lang = User::getLang();
-  }
-
-  private function openFile($path) {
-    foreach($this->cache as $k => $v) {
-      if($k == $this->lang) {
-        return $v;
-      }
+    public function __construct() {
+      $this->lang = User::getLang();
     }
-    $file_content = include $path;
-    $this->cache[$this->lang] = $file_content;
-    return $file_content;
-  }
 
-  /**
-   * @desc Функция для локализации.
-   * @return Строка с языком, указанным выше.
-   *
-   */
-    public function get($word) {
-      $lang_file_path = SITE_ROOT.'application/i18n/'.$this->lang.'.php';
-      $lang_file = $this->openFile($lang_file_path);
-      if(is_array($word)) {
-        foreach($word as $v) {
-          if(!empty($lang_file[$v])) {
-            $arr[$v] = $lang_file[$v];
-          }
+    private function openFile($path) {
+      foreach($this->cache as $k => $v) {
+        if($k == $this->lang) {
+          return $v;
         }
-        return $arr;
-      } 
-      if(!empty($lang_file[$word])) {
-        return $lang_file[$word];
       }
-      return '';
+      $file_content = include $path;
+      $this->cache[$this->lang] = $file_content;
+      return $file_content;
     }
 
+    /**
+     * @desc Функция для локализации.
+     * @return Строка с языком, указанным выше.
+     *
+     */
+      public function get($word) {
+        $lang_file_path = SITE_ROOT.'application/i18n/'.$this->lang.'.php';
+        $lang_file = $this->openFile($lang_file_path);
+        if(is_array($word)) {
+          foreach($word as $v) {
+            if(!empty($lang_file[$v])) {
+              $arr[$v] = $lang_file[$v];
+            }
+          }
+          return $arr;
+        } 
+        if(!empty($lang_file[$word])) {
+          return $lang_file[$word];
+        }
+        return '';
+      }
 
-}
-?>
+
+  }
+  ?>
