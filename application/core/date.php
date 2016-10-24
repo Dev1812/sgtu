4615a94b46141a2cf111f4d50fcadc830a74b743
queue_18 <timka_issaev@mail.ru>
2016-10-24 23:42:28 +0300

    overwrite class

--- application/core/date.php
+++ application/core/date.php
@@ -1,10 +1,9 @@
 <?php
 class Date{
 
-  public $i18n;
+  public $common;
 
   public function __construct() {
-    $this->i18n = new i18n;
     $this->common = new Common;
   }
 
@@ -13,6 +12,12 @@ class Date{
     $ts_date = date('d m Y');
     list($day, $month, $year) = explode(' ', $ts_date);
     return $day.' '.$this->common->getMonth($month).' '.$year;
+  }
+  public function getShortDate($ts) {
+    if(!is_numeric($ts)) return false;
+    $ts_date = date('d m');
+    list($day, $month) = explode(' ', $ts_date);
+    return $day.' '.$this->common->getShortMonth($month);
   }
 
   /**
@@ -24,18 +29,21 @@ class Date{
   public function parseTimestamp($ts) {
     if(!$ts || !is_numeric($ts)) return false;
 
+  
     $ts_date = date('h:i j n Y', $ts);
     list($ts_time, $ts_day, $ts_month, $ts_year) = explode(' ', $ts_date);
  
     $date = date('j n Y');
     list($day, $month, $year) = explode(' ', $date);
-    
+
+    $yesterday = $day-1;
+
     if($year != $ts_year) {
       return $ts_day.' '.$this->common->getShortMonth($ts_month).' '.$ts_year;//21 дек 2014
     } else {
       if($day == $ts_day) {
         return 'сегодня в '.$ts_time;//сегодня в 11:40
-      } elseif($day == $ts_day - 1) { //вчера
+      } elseif($yesterday == $ts_day) { //вчера
         return 'вчера в '.$ts_time;//вчера в в 11:40
       } else {
         return $ts_day.' '.$this->common->getShortMonth($ts_month).' в '.$ts_time;//9 дек в 14:32
